f7801405ce29ae238b64b2089c65d4e9807f9599
SeungChan An <dev.issea1015@gmail.com>
2016-05-01 00:30:33 +0900

    make practical simulator (CPU job)

--- simulator.cpp
+++ simulator.cpp
@@ -8,13 +8,19 @@ int main()
 {
     srand(clock());
     int memory_size = rand();
+    int numberOf_loop = rand();
     
     cout << "It's simulator." << endl;
     
     cout << "memory_size : " << memory_size << endl;
     void* memory = malloc(memory_size);
     
-    cout << endl;
+    for(int i=0 ;i<numberOf_loop; i++)
+    {
+        1*1;
+    }
+    
+    cout << "end" << endl;
     
     return 0;
 }
